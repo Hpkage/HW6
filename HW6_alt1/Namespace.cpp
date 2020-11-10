0682d0f81ca7934a9870b50de11b784ed71e0a01
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-11-09 19:29:16 -0900

    Namespace Trunc8

---
+++ HW6_alt1/Namespace.cpp
@@ -0,0 +1,13 @@
+#include "TestOne.h"
+#include "Namespace.h"
+#include <iostream>
+
+// trunc8 option, look in truncstruct.hpp for more detail
+void Trunc8::Button_CB(Fl_Widget*, void* userdata) {
+    char help[100];
+    std::string str;
+    strcpy_s(help, test_input->value());
+    str = help;
+    str = trunc8(str).str;
+    test_output->value(str.c_str());
+}
---
+++ HW6_alt1/Namespace.h
@@ -0,0 +1,13 @@
+#ifndef NAMESPACE_H
+#define NAMESPACE_H
+
+#include <iostream>
+#include "TestOne.h"
+#include "truncstruct.hpp"
+
+namespace Trunc8 {
+    // trunc8 option, look in truncstruct.hpp for more detail
+    void Button_CB(Fl_Widget*, void* userdata);
+}
+
+#endif NAMESPACE_H
