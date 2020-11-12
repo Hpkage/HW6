519ca55d3e646bb7f84610f32f342c56c50b6f95
Hiromi Kageyama <hpkageyama@alaska.edu>
2020-11-11 23:29:56 -0900

    Written Functions

--- HW6_main/Source.cpp
+++ HW6_main/Source.cpp
@@ -9,30 +9,79 @@
 #include <map>
 #include <random>
 #include <cmath>
+#include <stdlib.h>
 
-int main()
-{
+using std::random_device;
+
+int RandomBetweenU(int first, int last) {
     // Seed with a real random value, if available
-    std::random_device r;
+    random_device r;
 
     // Choose a random mean between 1 and 6
     std::default_random_engine e1(r());
-    std::uniform_int_distribution<int> uniform_dist(1, 6);
+    std::uniform_int_distribution<int> uniform_dist(first, last);
     int mean = uniform_dist(e1);
-    std::cout << "Randomly-chosen mean: " << mean << '\n';
+    return mean;
+}
+
+int RandomBetweenN(float first, float last) {
+    // Seed with a real random value, if available
+    random_device r;
+
+    // Choose a random mean between 1 and 6
+    std::default_random_engine e1(r());
+    std::normal_distribution<float> normal_dist(first, last);
+    int mean =abs((int) normal_dist(e1)) % (int) (last - first + 1) + first;
+    return mean;
+}
+
+int RandomBetween(int first, int last) {
+    srand(time(0));
+    int randNum = rand()%(last - first +1) + first;
+    return randNum;
+}
+
+void PrintDistribution(const std::map<int, int>& numbers) {
+    for (auto p : numbers) {
+        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
+            << p.first << ' ' << std::string (p.second / 200, '*') << '\n';
+    }
+}
+
+int main() {
+    // Seed with a real random value, if available
+    random_device r;
+
+    int first = 1;
+    int last = 6;
+    int mean = RandomBetweenU(first, last);
+    float mean2 = RandomBetweenN(first, last);
+    int mean3 = RandomBetween(first, last);
 
     // Generate a normal distribution around that mean
     std::seed_seq seed2{ r(), r(), r(), r(), r(), r(), r(), r() };
     std::mt19937 e2(seed2);
-    std::normal_distribution<> normal_dist(mean, 2);
+    std::normal_distribution<> normal_dist(mean2, 2);
+
+    std::uniform_int_distribution<> uniform_dist(mean, last);
 
     std::map<int, int> hist;
-    for (int n = 0; n < 10000; ++n) {
+    std::map<int, int> hist2;
+
+    for (int n = 0; n <10000; ++n) {
         ++hist[std::round(normal_dist(e2))];
     }
-    std::cout << "Normal distribution around " << mean << ":\n";
-    for (auto p : hist) {
-        std::cout << std::fixed << std::setprecision(1) << std::setw(2)
-            << p.first << ' ' << std::string(p.second / 200, '*') << '\n';
+    std::cout << "Normal distribution around " << mean2 << ":\n";
+    PrintDistribution(hist);
+    std::cout << std::endl;
+
+    for (int n = 0; n < 10000; ++n) {
+        ++hist2[std::round(uniform_dist(e2))];
     }
+    std::cout << "Uniform distribution around " << mean << ":\n";
+    PrintDistribution(hist2);
+
+    std::cout << "Rand distribution around " << mean3 << ":\n" << "\n";
+
+    std::cout << mean3 << std::endl; 
 }
